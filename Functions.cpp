6ee5062971072d490d22b53d1982a8087f7e4029
MateuszHater1992 <105385649+MateuszHater1992@users.noreply.github.com>
2022-06-23 01:41:25 +0200

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -1,15 +1,11 @@
 // swaps two variables efficiently
 // !! cant be const char* or floating number !! (works on string though)
-
 template<typename T, typename U>
-void swap(T &a, U &b)
-{
-	a^=b; b^=a;a^=b;
-}
+void swapE(T &fValue, U &sValue) { fValue^=sValue; sValue^=fValue; fValue^=sValue; }
+
 
 // checks for variable type
 // does not recognize rvalues and lvalues
-
 template <typename T> const char* varType(T&) { return "unknown"; }    // default
 template<> const char* varType(int&) { return "int"; }
 template<> const char* varType(short&) { return "short"; }
@@ -27,3 +23,27 @@ template<> const char* varType(unsigned char&) { return "unsigned char"; }
 template<> const char* varType(char*&) { return "char*"; }
 template<> const char* varType(signed char*&) { return "signed char*"; }
 template<> const char* varType(unsigned char*&) { return "unsigned char*"; }
+
+
+// logarithm of given value with a given base
+// obviously works only with numbers above 0
+unsigned int logN(unsigned int value, unsigned int base)
+{
+    return (n > r - 1) ? 1 +
+        logN(n / r, r) : 0;
+}
+
+
+// floors given value 
+int floorE(double v)
+{
+    int t;
+    if (v < 0) { t = (int)v + (-1); return t; }
+    else return v;
+}
+
+
+//using floor and logarithm function it outputs integer digits count
+int intLength(int a) {
+    return floorE(logN(a, 10)) + 1;
+}
